d068adf1e29d089909b1481fcc51434a2a79af17
vinicius <vini512033@gmail.com>
2022-08-30 10:44:18 -0300

    feat: gift

---
+++ components/ Gift.jsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Gift.module.css'
+
+export default function Gift () {
+  return (
+    <div className={styles.gift}>
+      <div className={styles.top}></div>
+      <div className={styles.body}></div>
+      <div className={styles.link1}></div>
+      <div className={styles.link2}></div>
+
+    </div>
+  )
+}
